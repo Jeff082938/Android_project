a7fa81eea0ccc6eaf2bd5f45686b3836f00a32da
詹子頡 <jeff0829381234@gmail.com>
2024-10-22 23:23:19 +0800

    經緯度

--- app/src/main/java/com/example/firstproject/MainActivity.java
+++ app/src/main/java/com/example/firstproject/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.firstproject;
 
+import android.os.Build;
 import android.os.Bundle;
-
+import androidx.annotation.NonNull;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.firstproject.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tvResult;
+    Button btShow;
     private ActivityMainBinding binding;
-
+    public static final String TAG = MainActivity.class.getSimpleName()+"My";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +45,71 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        //setContentView(R.layout.activity_main);
+        tvResult = findViewById(R.id.tv_result);
+        btShow = findViewById(R.id.bt_show);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, 100);
+        }
+
+    }
+
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        getLocal();
+    }
+
+    private void getLocal() {
+        /**沒有權限則返回*/
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        String localProvider = "";
+        /**知道位置後..*/
+        Location location = manager.getLastKnownLocation(localProvider) ;
+        if (location != null){
+            showLocation(location);
+        }else{
+            Log.d(TAG, "getLocal: ");
+            manager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, mListener);
+            manager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, mListener);
+        }
+    }
+    /**監聽位置變化*/
+    LocationListener mListener = new LocationListener() {
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+        }
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+        @Override
+        public void onLocationChanged(Location location) {
+            showLocation(location);
+        }
+    };
+
+    private void showLocation(Location location){
+
+        String address = "緯度："+String.format("%.5f", location.getLatitude()) +"經度："+String.format("%.5f", Math.abs(location.getLongitude()));
+        tvResult.setText(address);
+        btShow.setOnClickListener(view -> {
+            String url = "https://www.google.com/maps/@"+location.getLatitude()+","+Math.abs(location.getLongitude())+",15z";
+            Intent i = new Intent(Intent.ACTION_VIEW);
+            i.setData(Uri.parse(url));
+            startActivity(i);
+        });
+
     }
 
 }
